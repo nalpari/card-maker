86b241a9436c67b99964ca251ebde277b9c3bad6
yoosw <yoo32767@gmail.com>
2021-01-19 17:15:05 +0900

    router 추가
    라우터, 로그인, 로그아웃 추가

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/login/login';
+import Maker from './components/maker/maker';
 import styles from "./app.module.css";
 
 const App = ({authService}) => (
   <div className={styles.app}>
-    <Login authService={authService} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login authService={authService} />
+        </Route>
+        <Route path="/maker">
+          <Maker authService={authService} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </div>
 )
 
