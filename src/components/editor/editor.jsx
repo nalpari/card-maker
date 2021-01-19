97642de1c6ae3df2e4b4267efacc50af6c861474
yoosw <yoo32767@gmail.com>
2021-01-19 18:00:37 +0900

    스타일링 추가
    스타일링 추가

---
+++ src/components/editor/editor.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from "./editor.module.css";
+
+const Editor = () => {
+  return (
+    <section className={styles.editor}>
+      editor
+    </section>
+  );
+};
+
+export default Editor;
